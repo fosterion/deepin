e8d0bf5a9225c1adf091ab487f6f35e971d4b562
fosterion <32929118+fosterion@users.noreply.github.com>
2021-07-10 20:28:29 +0300

    Add yield implementation.

---
+++ src/Deepin.Collections/Iterators/YieldExample/CustomYield.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deepin.Collections.Iterators.YieldExample
+{
+    class CustomYield<T>
+    {
+        private T[] elements;
+
+        public CustomYield(T[] elements)
+        {
+            this.elements = elements;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                yield return elements[i];
+            }
+        }
+    }
+}
