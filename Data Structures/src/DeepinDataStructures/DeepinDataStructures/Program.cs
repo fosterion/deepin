ef8aa6e62f24bdb078c296a8fcfa2ae0d98bde1f
fosterion <32929118+fosterion@users.noreply.github.com>
2021-07-18 10:42:20 +0300

    Add new solutions.

---
+++ Data Structures/src/DeepinDataStructures/DeepinDataStructures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeepinDataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Main/src/Deepin.Collections/Interfaces/IEnumerableVsIQueryable/CustomExample.cs
@@ -0,0 +1,33 @@
+﻿using Deepin.Shared.Models.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deepin.Collections.Interfaces.IEnumerableVsIQueryable
+{
+    class CustomExample
+    {
+        private Book[] _books =
+        {
+            new Book("Book 1", "Author 1"),
+            new Book("Book 2", "Author 1"),
+            new Book("Book 3", "Author 2"),
+            new Book("Book 4", "Author 2"),
+            new Book("Book 5", "Author 2")
+        };
+
+        public void GetBooksAsIEnumerable()
+        {
+            IEnumerable<Book> books = _books;
+            var selected = books.Where(p => p.Author == "Author 2").ToList();
+        }
+
+        public void GetBooksAsIQueryable()
+        {
+            IQueryable<Book> books = _books;
+            var selected = books.Where(p => p.Author == "Author 2").ToList();
+        }
+    }
+}
